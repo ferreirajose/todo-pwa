628a113a0b83aa725d4eb45a5df9b4ecdae09981
José Ferreira <ferreirajosejp@gmail.com>
2024-05-22 12:33:04 -0300

    feat: todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,122 @@
-import { Component } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-pwa';
+export class AppComponent implements OnInit {
+
+  isOnline: boolean;
+  modalVersion: boolean;
+  modalPwaEvent: any;
+  modalPwaPlatform: string|undefined;
+
+  constructor(
+    private platform: Platform,
+    private swUpdate: SwUpdate
+  ) {
+    this.isOnline = false;
+    this.modalVersion = false;
+  }
+
+  public ngOnInit(): void {
+    this.updateOnlineStatus();
+    this.showTasks();
+
+    window.addEventListener('online',  this.updateOnlineStatus.bind(this));
+    window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.pipe(
+        filter((evt: any): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+        map((evt: any) => {
+          console.info(`currentVersion=[${evt.currentVersion} | latestVersion=[${evt.latestVersion}]`);
+          this.modalVersion = true;
+        }),
+      );
+    }
+
+    this.loadModalPwa();
+  }
+
+  private updateOnlineStatus(): void {
+    this.isOnline = window.navigator.onLine;
+    console.info(`isOnline=[${this.isOnline}]`);
+  }
+
+  public updateVersion(): void {
+    this.modalVersion = false;
+    window.location.reload();
+  }
+
+  public closeVersion(): void {
+    this.modalVersion = false;
+  }
+
+  private loadModalPwa(): void {
+    if (this.platform.ANDROID) {
+      window.addEventListener('beforeinstallprompt', (event: any) => {
+        event.preventDefault();
+        this.modalPwaEvent = event;
+        this.modalPwaPlatform = 'ANDROID';
+      });
+    }
+
+    if (this.platform.IOS && this.platform.SAFARI) {
+      const isInStandaloneMode = ('standalone' in window.navigator) && ((window.navigator as any)['standalone']);
+      if (!isInStandaloneMode) {
+        this.modalPwaPlatform = 'IOS';
+      }
+    }
+  }
+
+  public addToHomeScreen(): void {
+    this.modalPwaEvent.prompt();
+    this.modalPwaPlatform = undefined;
+  }
+
+  public closePwa(): void {
+    this.modalPwaPlatform = undefined;
+  }
+
+
+  ///
+
+  task: string = '';
+  tasks: { name: string, completed: boolean }[] = [];
+
+  addTask() {
+    if (this.task.trim() === '') {
+      alert("you must write something!");
+      return;
+    }
+    this.tasks.push({ name: this.task, completed: false });
+    this.task = '';
+    this.saveData();
+  }
+
+  deleteTask(taskToDelete: { name: string, completed: boolean }) {
+    this.tasks = this.tasks.filter(task => task !== taskToDelete);
+    this.saveData();
+  }
+
+  toggleTaskCompletion(task: { name: string, completed: boolean }) {
+    task.completed = !task.completed;
+    this.saveData();
+  }
+
+  saveData() {
+    localStorage.setItem("tasks", JSON.stringify(this.tasks));
+  }
+
+  showTasks() {
+    const savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+      this.tasks = JSON.parse(savedTasks);
+    }
+  }
 }
